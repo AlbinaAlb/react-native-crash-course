9cd1e8494df9a1ea14eaf0ca72b883a961c18d15
Albina <albina@Albinas-MacBook-Pro.local>
2025-03-12 15:03:16 +0100

    setting fonts and icons from Figma

--- app/index.jsx
+++ app/index.jsx
@@ -2,10 +2,11 @@ import { StatusBar } from 'expo-status-bar'
 import { Link } from 'expo-router'
 import { Text, View } from 'react-native'
 
+
 export default function Index() {
   return (
-    <View className='flex-1 items-center justify-center bg-white'>
-      <Text className='text-3xl'>Aora!</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl font-pblack">Aora!</Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{ color: 'blue' }}>
         Go to Profile
