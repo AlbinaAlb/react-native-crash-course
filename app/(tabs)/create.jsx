2d553258eed304da8f0d6d7be4015fb8e93e0973
Albina <albina@Albinas-MacBook-Pro.local>
2025-03-13 14:28:33 +0100

    file & folder structure

---
+++ app/(tabs)/create.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+//будет создавать и добавлять видео на платформу
+const Create = () => {
+  return (
+    <View>
+      <Text>Create</Text>
+    </View>
+  )
+}
+
+export default Create
